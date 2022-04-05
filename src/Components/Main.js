8d34fa6e686bfb3e8f96da624ae5a57fc61ae1bd
Kevin Liang <kevinliang@kevinliang.attlocal.net>
2022-04-04 17:03:07 -0700

    Initial commit

---
+++ src/Components/Main.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Main = () => {
+
+    return (
+        <section>
+            <div className="title">Orbit Protocol is a new generation on-chain lending protocol that enables future income streams as collateral. We expect the number of on-chain businesses that generate significant revenue to drastically increase and drive demand for an alternative source of capital. Liquidation-free loans and improved capital efficiency for on-chain entities.</div>
+            <div className="socials">
+                <div className="title--tag">Coming soon. To learn more or to discuss ideas, please see our documentation and/or message us.</div>
+            </div>
+
+            <div className="tag--box">
+                <a className="tag" href="https://twitter.com/juicefinance">Twitter</a>
+                <a className="tag" href="https://kliang9715.gitbook.io/product-docs/getting-started/introduction">Documentation</a>
+            </div>
+        </section>
+    )
+}
+
+export default Main;
