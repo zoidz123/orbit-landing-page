84b92d7d47587529efdfb7d78c59b9cf683720bc
Kevin Liang <kevinliang@kevinliang.attlocal.net>
2022-04-04 20:09:52 -0700

    Changed more wording

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -27,7 +27,7 @@ const Main = () => {
                     </div>
                     :
                     <div className="small--wrapper">
-                        <div className="title">Orbit Protocol is a new generation on-chain lending protocol that enables future income streams as collateral. We expect the number of on-chain businesses that generate significant revenue to drastically increase and drive demand for an alternative source of capital. Liquidation-free loans and improved capital efficiency for on-chain entities.</div>
+                        <div className="title">Orbit Protocol is a next generation on-chain lending protocol that enables future income streams as collateral. We expect the number of on-chain businesses that generate significant revenue to drastically increase and drive demand for an alternative source of capital. Liquidation-free loans and improved capital efficiency for on-chain entities.</div>
                         <div className="socials">
                             <div className="title--tag">Coming soon. To learn more or to discuss ideas, please see our documentation and/or message us.</div>
                         </div>
